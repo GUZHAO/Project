8e9e03a7935349da65b97992a2be8239493d9198
GUZHAO <guzhao1990@gmail.com>
2018-04-07 20:05:24 -0400

    end of day update

--- GetTable.py
+++ GetTable.py
@@ -21,21 +21,25 @@ def randw(writefile, readfile):
                     tb.write(fcont.strip() + '\n')
 
 
-# popc
-randw('//cifs2/homedir$/Office/Projects/Report Inventory/result/POPC.txt',
-      '//cifs2/homedir$/Office/Projects/Report Inventory/POPC_Oracle.txt')
-# rvu
-randw('//cifs2/homedir$/Office/Projects/Report Inventory/result/RVUMedOnc.txt',
-      '//cifs2/homedir$/Office/Projects/Report Inventory/RVU Adult MedOncFinal.txt')
-# new pt 5 days
-randw('//cifs2/homedir$/Office/Projects/Report Inventory/result/NP5D.txt',
-      '//cifs2/homedir$/Office/Projects/Report Inventory/NewPt5Days.txt')
-# new pt 5 days 2
-randw('//cifs2/homedir$/Office/Projects/Report Inventory/result/NP5D2.txt',
-      '//cifs2/homedir$/Office/Projects/Report Inventory/NewPt5Days2.txt')
-# new pt next day
-randw('//cifs2/homedir$/Office/Projects/Report Inventory/result/NPND.txt',
-      '//cifs2/homedir$/Office/Projects/Report Inventory/NewPtNextDay.txt')
-# new pt next day 2
-randw('//cifs2/homedir$/Office/Projects/Report Inventory/result/NPND2.txt',
-      '//cifs2/homedir$/Office/Projects/Report Inventory/NewPtNextDay2.txt')
+#POPC ACP
+randw('//cifs2/coba$/Ad-Hoc Data Requests/POPC Advanced Care Planning QI Project/POPC_ACP.txt',
+      '//cifs2/coba$/Ad-Hoc Data Requests/POPC Advanced Care Planning QI Project/AdvancedCarePlanCodeDFCI03192018.txt')
+
+# # popc
+# randw('//cifs2/homedir$/Office/Projects/Report Inventory/result/POPC.txt',
+#       '//cifs2/homedir$/Office/Projects/Report Inventory/POPC_Oracle.txt')
+# # rvu
+# randw('//cifs2/homedir$/Office/Projects/Report Inventory/result/RVUMedOnc.txt',
+#       '//cifs2/homedir$/Office/Projects/Report Inventory/RVU Adult MedOncFinal.txt')
+# # new pt 5 days
+# randw('//cifs2/homedir$/Office/Projects/Report Inventory/result/NP5D.txt',
+#       '//cifs2/homedir$/Office/Projects/Report Inventory/NewPt5Days.txt')
+# # new pt 5 days 2
+# randw('//cifs2/homedir$/Office/Projects/Report Inventory/result/NP5D2.txt',
+#       '//cifs2/homedir$/Office/Projects/Report Inventory/NewPt5Days2.txt')
+# # new pt next day
+# randw('//cifs2/homedir$/Office/Projects/Report Inventory/result/NPND.txt',
+#       '//cifs2/homedir$/Office/Projects/Report Inventory/NewPtNextDay.txt')
+# # new pt next day 2
+# randw('//cifs2/homedir$/Office/Projects/Report Inventory/result/NPND2.txt',
+#       '//cifs2/homedir$/Office/Projects/Report Inventory/NewPtNextDay2.txt')
